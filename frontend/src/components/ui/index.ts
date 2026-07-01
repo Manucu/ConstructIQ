33d9ebbdddb976b7191577d108890e0638758f76
Emanuel Niculai <manuku_niculai@yaho.com>
2026-07-01 01:03:26 +0100

    Add UI component exports

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,5 @@
+export { AppButton } from "./AppButton";
+export { AppInput } from "./AppInput";
+export { AppCard } from "./AppCard";
+export { AppStatCard } from "./AppStatCard";
+export { AppLogoCard } from "./AppLogoCard";
